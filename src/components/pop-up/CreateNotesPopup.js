c5884326b772fcb7febef80b5a4ab90c8932b1fc
Harsh raj <harsh970raj@gmail.com>
2024-02-06 07:49:12 +0530

    changes

--- src/components/pop-up/CreateNotesPopup.js
+++ src/components/pop-up/CreateNotesPopup.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./CreateNotesPopup.css";
 
-function CreateNotesPopup({ groupNamesParent, setGroupNamesParent, onClose }) {
+function CreateNotesPopup({ groupNamesParent, setGroupNamesParent, onClose, }) {
   const [groupName, setGroupName] = useState("");
   const [bgColor, setBgColor] = useState("");
 
@@ -16,16 +16,32 @@ function CreateNotesPopup({ groupNamesParent, setGroupNamesParent, onClose }) {
 
   const saveName = () => {
     const newGroup = { name: groupName, color: bgColor };
+    onClose();
     setGroupNamesParent([...groupNamesParent, newGroup]);
+
     localStorage.setItem(
       "groupNames",
       JSON.stringify([...groupNamesParent, newGroup])
     );
-    onClose();
+
+  };
+  
+  const refs = useRef(null);
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick, true);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick, true);
+    };
+  }, []);
+
+  const handleOutsideClick = (e) => {
+    if (!refs.current.contains(e.target)) {
+      onClose();
+    }
   };
 
   return (
-    <div className="popup">
+    <div className="popup" ref={refs}>
       <div className="popup__title">Create New Group</div>
       <div className="popup__input">
         <span>Group Name</span>
@@ -40,39 +56,33 @@ function CreateNotesPopup({ groupNamesParent, setGroupNamesParent, onClose }) {
         <span >Group Color</span>
         <div className="popup__color__input__color">
           <div
-            className={`popup__color__input__color__1 ${
-              bgColor === "rgb(179, 139, 250)" ? `highlight` : null
-            }`}
+            className={`popup__color__input__color__1 ${bgColor === "rgb(179, 139, 250)" ? `highlight` : null
+              }`}
             onClick={handleColor}
           ></div>
           <div
-            className={`popup__color__input__color__2 ${
-              bgColor === "rgb(255, 121, 242)" ? `highlight` : null
-            }`}
+            className={`popup__color__input__color__2 ${bgColor === "rgb(255, 121, 242)" ? `highlight` : null
+              }`}
             onClick={handleColor}
           ></div>
           <div
-            className={`popup__color__input__color__3 ${
-              bgColor === "rgb(67, 230, 252)" ? `highlight` : null
-            }`}
+            className={`popup__color__input__color__3 ${bgColor === "rgb(67, 230, 252)" ? `highlight` : null
+              }`}
             onClick={handleColor}
           ></div>
           <div
-            className={`popup__color__input__color__4 ${
-              bgColor === "rgb(241, 149, 118)" ? `highlight` : null
-            }`}
+            className={`popup__color__input__color__4 ${bgColor === "rgb(241, 149, 118)" ? `highlight` : null
+              }`}
             onClick={handleColor}
           ></div>
           <div
-            className={`popup__color__input__color__5 ${
-              bgColor === "rgb(0, 71, 255)" ? `highlight` : null
-            }`}
+            className={`popup__color__input__color__5 ${bgColor === "rgb(0, 71, 255)" ? `highlight` : null
+              }`}
             onClick={handleColor}
           ></div>
           <div
-            className={`popup__color__input__color__6 ${
-              bgColor === "rgb(102, 145, 255)" ? `highlight` : null
-            }`}
+            className={`popup__color__input__color__6 ${bgColor === "rgb(102, 145, 255)" ? `highlight` : null
+              }`}
             onClick={handleColor}
           ></div>
         </div>
